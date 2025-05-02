7d272ed2de9687844af63895d549821d8e3702f7
Gihan Idris <idrisgihan28@gmail.com>
2025-05-02 06:05:52 -0400

    Task14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,45 +18,68 @@ public_users.post("/register", (req, res) => {
       return res.status(409).json({ message: "Username already exists." });
     }
   
-    // Register the new user
     users.push({ username, password });
     return res.status(200).json({ message: "User registered successfully!" });
   });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.send(JSON.stringify(books,null,4));
+public_users.get('/', async function (req, res) {
+    try {
+    const allBooks = await getBooks();
+    return res.send(JSON.stringify(allBooks,null,4));
+    } catch(err) {
+    return res.status(500).json({ message: "Failed to fetch all books", error: err });
+    }
 });
 
+async function getBooks() {
+    return books;
+}
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
+ try {
   const isbn = req.params.isbn;
-  const book = books[isbn];
+  const book = await getBookByISBN(isbn);
 
   if (book) {
     return res.status(200).json(book);
   } else {
     return res.status(404).json({ message: "Book not found" });
   }
-});
+} catch(err) {
+    return res.status(500).json({message: "Could not retrieve book", error: err});
+}
+})
+
+async function getBookByISBN(isbn) {
+    return books[isbn];
+}
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
+try {
   const author = req.params.author;
-  let matchingBooks = [];
-
-  for (let key in books) {
-    if (books[key].author === author) {
-        matchingBooks.push(books[key]);
-    }
-  }
+  let matchingBooks = await getBooksByAuthor(author)
 
   if (matchingBooks.length > 0) {
     res.status(200).json(matchingBooks);
   } else {
     res.status(404).json({ message : "No books found for author " + author});
   }
+} catch(err) {
+    return res.status(500).json({ message: "Couldnt retrieve book", error: err });
+}
 });
+
+async function getBooksByAuthor(author) {
+    const books = [];
+    for (let key in books) {
+        if (books[key].author === author) {
+            books.push(books[key]);
+        }
+      }
+      return books;
+}
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
